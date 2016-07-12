9bbe951ff5d14957de02a13fff1edc469a827a40
Dennis Choi <dennis.choi7@gmail.com>
2016-07-11 23:20:15 -0400

    - Update place through URL and retrieve the data from open weather as JSON and convert 		- Tested and works - Compass for the wind direction function - Rounding of the temperature function

--- assets/js/forecast.js
+++ assets/js/forecast.js
@@ -1,4 +1,5 @@
 var APPID = "80341d69375e56322835d5bd041bf7e5";
+var forecast = [];
 var day;
 var loc;
 var icon;
@@ -16,22 +17,77 @@ var direction2;
 
 
 
+
+function updateByPlace(place) {
+    var url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+      "q=" + place +
+      "&mode=json&units=metric" +
+      // PRONE TO CHANGE FROM 2 to 7
+      "&cnt=2" +
+      "&APPID=" + APPID;
+    sendRequest(url)
+}
+
+function sendRequest(url) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var data = JSON.parse(xmlhttp.responseText);
+            var forecast = {};
+            forecast.icon = data.list[0].weather[0].icon;
+            forecast.icon2 = data.list[1].weather[0].icon;
+            forecast.temp = data.list[0].temp.day;
+            forecast.temp2 = data.list[1].temp.day;
+            forecast.name = data.city.name;
+            forecast.name2 = data.city.name;
+            forecast.humidity = data.list[0].humidity;
+            forecast.humidity2 = data.list[1].humidity;
+            forecast.speed = data.list[0].speed;
+            forecast.speed2 = data.list[1].speed;
+            forecast.deg = data.list[0].deg;
+            forecast.deg2 = data.list[1].deg;
+
+            update(forecast)
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+// PRONE TO CHANGE...
 function update(forecast) {
-    temp.innerHTML = list[0].temp;
-    temp2.innerHTML = list[1].temp;
-    loc.innerHTML = city.name;
-    loc2.innerHTML = city.name;
-    humidity.innerHTML = list[0].humidity;
-    humidity2.innerHTML = list[1].humidity;
-    wind.innerHTML = list[0].speed;
-    wind2.innerHTML = list[1].speed;
-    direction.innerHTML = list[0].deg;
-    direction2.innerHTML = list[1].deg
-    icon.src = "assets/img/" + list[0].weather.icon + ".png";
-    icon2.src = "assets/img/" + list[1].weather.icon + ".png";
+    temp.innerHTML = roundTemp(forecast.temp);
+    temp2.innerHTML = roundTemp(forecast.temp2);
+    loc.innerHTML = forecast.name;
+    loc2.innerHTML = forecast.name2;
+    humidity.innerHTML = forecast.humidity;
+    humidity2.innerHTML = forecast.humidity2;
+    wind.innerHTML = forecast.speed;
+    wind2.innerHTML = forecast.speed2;
+    direction.innerHTML = compassDirection(forecast.deg);
+    direction2.innerHTML = compassDirection(forecast.deg2);
+    icon.src = "assets/img/" + forecast.icon + ".png";
+    icon2.src = "assets/img/" + forecast.icon2 + ".png";
 
 }
 
+function roundTemp(t) {
+    return Math.round(t);
+}
+
+function compassDirection(degree) {
+    var range = 360/8;
+    var low = 360 - range/2;
+    var high = (low + range) % 360;
+    var angles = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    for (i in angles) {
+        if (degree >= low && degree < high)
+            return angles[i];
+        low = (low + range) % 360;
+        high = (high + range) % 360;
+    }
+}
 
 window.onload = function() {
   loc = document.getElementById('location')
@@ -48,5 +104,7 @@ window.onload = function() {
   direction2 = document.getElementById('direction2')
   temp2 = document.getElementById('temperature2')
 
-  update(forecast);
+
+
+  updateByPlace("London");
 }
